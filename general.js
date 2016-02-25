cb55a9ebf0693503aa620d1dead0a90634ecd0c5
Nguyen Quang Huy <nguyen_huy@Nguyens-MacBook-Air-2.local>
2016-02-26 02:44:47 +0900

    huy_change responsive header

--- general.js
+++ general.js
@@ -1,11 +1,16 @@
 $(window).scroll(function() {
-  if ($(document).scrollTop() > 50) {
+	var windowWidth = $(window).width();
+	console.log(windowWidth);
+	if(windowWidth > 991)
+	{	
+  if ($(document).scrollTop() > 50 && windowWidth > 991) {
     $('.navbar-nav li a ').css('line-height','20px');
-    $('.pk-logo-navbar').css('padding','5px 15px');
+    $('.pk-logo-navbar img').css('margin','5px 15px');
     $('#custom-search-input').css('margin-top','10px');
   }else {	
   	$('.navbar-nav li a').css('line-height','40px');
-  	$('.pk-logo-navbar').css('padding','15px 15px');  	
+  	$('.pk-logo-navbar img').css('margin','15px 15px');  	
   	$('#custom-search-input').css('margin-top','20px');
   }
+}
 });
