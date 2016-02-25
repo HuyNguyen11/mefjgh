9419a9552f05bede35a1db6319203b72da4a2cb1
huy_nguyen <huy_nguyen@irep.co.jp>
2016-02-25 19:46:39 +0900

    remove unused file

--- laravel/resources/views/welcome.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.layout')
-    
-@section('content')
-    <ul>
-    @foreach ($songs as $song)
-        <li><a href="/songs/{{$song->id_song}}">{{$song->name}}</a> -- {{$song->name_viet}}</li>
-    @endforeach
-    </ul>
-@stop
